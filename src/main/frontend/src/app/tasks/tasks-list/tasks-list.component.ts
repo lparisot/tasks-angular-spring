e5d2e96c49026f19cc076b1829f1645951635ae4
L.P <parisot.laurent@gmail.com>
2017-11-23 13:24:41 +0100

    Add Angular part

---
+++ src/main/frontend/src/app/tasks/tasks-list/tasks-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'app/tasks/task.model';
+
+@Component({
+  selector: 'app-tasks-list',
+  templateUrl: './tasks-list.component.html',
+  styleUrls: ['./tasks-list.component.css']
+})
+export class TasksListComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.tasks.push(new Task(1, "Task 1", true, "22/11/2017"));
+    this.tasks.push(new Task(2, "Task 2", false, "22/11/2017"));
+    this.tasks.push(new Task(3, "Task 3", false, "22/11/2017"));
+  }
+
+}
